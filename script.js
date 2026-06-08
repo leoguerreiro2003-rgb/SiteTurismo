1458245d0cdeefeae44e8b77e717a3b65fab69d7
leandrofilipeguerreiropinto <leoguerreiro2003@gmail.com>
2026-06-08 10:39:10 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let btnNext = document.querySelector(".next")
+let btnback = document.querySelector(".back")
+
+let container = document.querySelector(".container")
+let list = document.querySelector(".contaner .list")
+let thumb = document.querySelector(".container .thumb")
+
+btnNext.onclick = () => moveItemsOnClick("next")
+btnNext.onclick = () => moveItemsOnClick("back")
+
+function moveItemsOnClick(type) {
+
+}
